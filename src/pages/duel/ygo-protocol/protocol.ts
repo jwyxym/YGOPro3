069cb0bbddc05305c8a14158c0f81d6225b68afa
jwyxym <jwyxym@126.com>
2026-03-11 15:45:14 +0800

    Update protocol.ts

--- src/pages/duel/ygo-protocol/protocol.ts
+++ src/pages/duel/ygo-protocol/protocol.ts
@@ -39,40 +39,39 @@ class Protocol {
 				return;
 			switch (protocol) {
 				case ERROR.DECKERROR:
-					case ERROR.DECKERROR:
-						const flag = code >> 28;
-						const id = code & mainGame.max_card_id;
-						let str;
-						switch (flag) {
-							case ERROR.LFLIST:
-								str = mainGame.get.strings.system(1407, mainGame.get.name(id));
-								break;
-							case ERROR.OCGONLY:
-								str = mainGame.get.strings.system(1413, mainGame.get.name(id));
-								break;
-							case ERROR.TCGONLY:
-								str = mainGame.get.strings.system(1414, mainGame.get.name(id));
-								break;
-							case ERROR.UNKNOWNCARD:
-								str = mainGame.get.strings.system(1415, [mainGame.get.name(id), id]);
-								break;
-							case ERROR.CARDCOUNT:
-								str = mainGame.get.strings.system(1416,mainGame.get.name(id));
-								break;
-							case ERROR.MAINCOUNT:
-								str = mainGame.get.strings.system(1417, id);
-								break;
-							case ERROR.EXTRACOUNT:
-								str = mainGame.get.strings.system(id > 0 ? 1418 : 1420, mainGame.get.name(id));
-								break;
-							case ERROR.SIDECOUNT:
-								str = mainGame.get.strings.system(1419, id);
-								break;
-							default:
-								str = mainGame.get.text(I18N_KEYS.UNKNOW);
-								break;
-						}
-						break;
+					const flag = code >> 28;
+					const id = code & mainGame.max_card_id;
+					let str;
+					switch (flag) {
+						case ERROR.LFLIST:
+							str = mainGame.get.strings.system(1407, mainGame.get.name(id));
+							break;
+						case ERROR.OCGONLY:
+							str = mainGame.get.strings.system(1413, mainGame.get.name(id));
+							break;
+						case ERROR.TCGONLY:
+							str = mainGame.get.strings.system(1414, mainGame.get.name(id));
+							break;
+						case ERROR.UNKNOWNCARD:
+							str = mainGame.get.strings.system(1415, [mainGame.get.name(id), id]);
+							break;
+						case ERROR.CARDCOUNT:
+							str = mainGame.get.strings.system(1416,mainGame.get.name(id));
+							break;
+						case ERROR.MAINCOUNT:
+							str = mainGame.get.strings.system(1417, id);
+							break;
+						case ERROR.EXTRACOUNT:
+							str = mainGame.get.strings.system(id > 0 ? 1418 : 1420, mainGame.get.name(id));
+							break;
+						case ERROR.SIDECOUNT:
+							str = mainGame.get.strings.system(1419, id);
+							break;
+						default:
+							str = mainGame.get.text(I18N_KEYS.UNKNOW);
+							break;
+					}
+					break;
 			}
 		}],
 	]);
