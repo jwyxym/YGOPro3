ed4246674329d7d47ee1c2b21cac0e16dbbc4934
jwyxym <jwyxym@126.com>
2026-04-24 16:08:59 +0800

    修复流水线重命名bug

--- .ci/rename.sh
+++ .ci/rename.sh
@@ -2,7 +2,10 @@ find . -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
     file=$(find "$dir" -maxdepth 1 -type f | head -n 1)
     [ -n "$file" ] || continue
 
-    newname="$(basename "$dir")"
+    filename=$(basename "$file")
+    dirname=$(basename "$dir")
 
-    mv "$file" "$dir/$newname"
+    if [ "$filename" != "$dirname" ]; then
+        mv "$file" "$dir/$dirname"
+    fi
 done
