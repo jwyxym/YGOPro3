1f73fbc4f43bbea7f6ef3b2da93fce8bef34d4c0
jwyxym <jwyxym@126.com>
2026-04-27 17:15:13 +0800

    修复响度条bug

--- src/pages/ui/slider.vue
+++ src/pages/ui/slider.vue
@@ -1,61 +1,76 @@
 <template>
-	<div class = 'slider' ref = 'slider' :style = "{ '--width' : `${page.x}px`, '--base_width' : `${page.width}px` }">
+	<div
+		class = 'slider'
+		:style = "{ '--width' : `${page.x}px`, '--base_width' : `${page.width}px` }"
+		@touchstart = 'page.touchstart'
+		@mousedown = 'page.mousedown'
+	>
 		<div></div>
 		<div></div>
 	</div>
 </template>
 <script setup lang = 'ts'>
-	import { onBeforeMount, onUnmounted, reactive, ref } from 'vue';
+	import { onBeforeMount, onUnmounted, reactive } from 'vue';
 	import GLOBAL from '@/script/scale';
 
-	const slider = ref<HTMLElement | null>(null);
 	const page = reactive({
 		x : 0,
 		width : 1200,
+		left : 0,
 		on : false,
-		start : (target : HTMLElement, x : number) => {
-			if (slider.value?.contains(target)) {
-				page.x = Math.max(0, (x / GLOBAL.SCALE) - GLOBAL.LEFT * 2 - 30);
-				emit('dragging', Math.min(1, page.x / page.width));
-				page.on = true;
-			}
+		clamp : (v : number) : number => Math.min(1, Math.max(0, v)),
+		computed : (x : number) : number => (x - page.left) / (page.width * GLOBAL.SCALE),
+		start : (x : number) => {
+			const ratio = page.clamp(page.computed(x));
+			page.x = page.width * ratio;
+			emit('dragging', ratio);
+			page.on = true;
 		},
 		move : (x : number) => {
 			if (page.on) {
-				page.x = Math.max(0, (x / GLOBAL.SCALE) - GLOBAL.LEFT * 2 - 30);
-				emit('dragging', Math.min(1, page.x / page.width));
+				const ratio = page.clamp(page.computed(x));
+				page.x = page.width * ratio;
+				emit('dragging', ratio);
 			}
 		},
 		end : (x : number) => {
 			if (page.on) {
-				page.x = Math.max(0, (x / GLOBAL.SCALE) - GLOBAL.LEFT * 2 - 30);
-				emit('drag_end', Math.min(1, Math.min(1, page.x / page.width)));
+				const ratio = page.clamp(page.computed(x));
+				page.x = page.width * ratio;
+				emit('dragging', ratio);
 				page.on = false;
 			}
 		},
-		touchstart : (e : TouchEvent) => page.start(e.target as HTMLElement, e.touches[0].clientX),
+		touchstart : (e : TouchEvent) => {
+			const target = e.target as HTMLElement;
+			if (!page.left)
+				page.left = target.getBoundingClientRect().left ?? 0;
+			page.start(e.touches[0].clientX);
+		},
 		touchmove : (e : TouchEvent) => page.move(e.touches[0].clientX),
 		touchend : (e : TouchEvent) => page.end(e.touches[0].clientX),
-		mousedown : (e : MouseEvent) => e.button === 2 ? false : page.start(e.target as HTMLElement, e.clientX),
-		mousemove : (e : MouseEvent) => page.move(e.clientX),
+		mousedown : (e : MouseEvent) => {
+			const target = e.target as HTMLElement;
+			if (!page.left)
+				page.left = target.getBoundingClientRect().left ?? 0;
+			if (e.button !== 2)
+				page.start(e.clientX);
+		},
+		mousemove : (e : MouseEvent) => page.move( e.clientX),
 		mouseup : (e : MouseEvent) => page.end(e.clientX)
 	});
 
 	onBeforeMount(async () => {
 		page.x = props.x * page.width;
-		window.addEventListener('mousedown', page.mousedown);
 		window.addEventListener('mousemove', page.mousemove);
 		window.addEventListener('mouseup', page.mouseup);
-		window.addEventListener('touchstart', page.touchstart);
 		window.addEventListener('touchmove', page.touchmove);
 		window.addEventListener('touchend', page.touchend);
 	});
 
 	onUnmounted(() => {
-		window.removeEventListener('mousedown', page.mousedown);
 		window.removeEventListener('mousemove', page.mousemove);
 		window.removeEventListener('mouseup', page.mouseup);
-		window.removeEventListener('touchstart', page.touchstart);
 		window.removeEventListener('touchmove', page.touchmove);
 		window.removeEventListener('touchend', page.touchend);
 	});
@@ -73,6 +88,7 @@
 	.slider {
 		position: relative;
 		height: 20px;
+		pointer-events: none;
 		> div {
 			position: absolute;
 			left: 0;
@@ -83,11 +99,13 @@
 		> div:first-child {
 			background-color: white;
 			width: var(--base_width);
+			pointer-events: initial;
 		}
 		> div:last-child {
 			background-color: #397bfe;
 			width: min(var(--width), var(--base_width));
 			z-index: 999;
+			pointer-events: none;
 		}
 	}
 </style>
