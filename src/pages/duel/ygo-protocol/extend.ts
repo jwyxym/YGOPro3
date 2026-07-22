84fe4d3293e7fc35cc7faa59729ca88edfcf0cfc
jwyxym <jwyxym@126.com>
2026-07-22 13:01:36 +0800

    Update extend.ts

--- src/pages/duel/ygo-protocol/extend.ts
+++ src/pages/duel/ygo-protocol/extend.ts
@@ -1,8 +1,34 @@
 import dg from '@/script/dglab';
-import { MSG } from './network';
 
-const extend = new Map<number, Function>([]);
-extend.set(MSG.DAMAGE, dg.happen);
-extend.set(MSG.PAY_LPCOST, dg.happen);
+import connect from '@/pages/duel/connect';
+
+import { MSG } from './network';
+import Msg from './msg';
+
+class Extend {
+	to = {
+		player : (player : number) : 0 | 1 => {
+			player = player > 0 ? 1 : 0;
+			return (connect.duel.is_first ? player : 1 - player) as 0 | 1 ;
+		}
+	};
+
+	private content = new Map<number, (msg : Msg) => Promise<void>>([
+		[MSG.DAMAGE, async (msg : Msg) => {
+			const tp = this.to.player(msg.read.uint8() ?? 0);
+			if (tp) return;
+			const val = msg.read.int32();
+			await dg.happen();
+		}]
+	]);
+
+	constructor () {
+		this.content.set(MSG.PAY_LPCOST, this.content.get(MSG.DAMAGE)!);
+	};
+
+	get = (i : number) => this.content.get(i);
+};
+
+const extend = new Extend();
 
 export default extend;
