aef1cd8c77acfb83d101989856d0e0416149b5fb
jwyxym <jwyxym@126.com>
2026-09-05 01:41:12 +0800

    update ygopru

--- src-tauri/tauri-plugin-ygopro3/crates/ygopro3_single_duel/src/lib.rs
+++ src-tauri/tauri-plugin-ygopro3/crates/ygopro3_single_duel/src/lib.rs
@@ -1,48 +1,37 @@
-use anyhow::{Result, Error, anyhow};
-use binrw::BinRead;
-use futures::SinkExt;
-use hashbrown::HashMap;
-use parking_lot::Mutex;
-use tokio_stream::StreamExt;
-use tokio_util::codec::LengthDelimitedCodec;
-use tokio::{
-	net::TcpListener,
-	sync::oneshot
-};
+use anyhow::{Error, Result, anyhow};
+use parking_lot::{Mutex, MutexGuard};
 use std::{
-	io::Cursor,
+	borrow::Cow,
+	ffi::{CStr, c_char, c_int},
+	fs::read,
+	ptr::null_mut,
 	sync::OnceLock,
 	thread::JoinHandle,
 	time::Duration,
-	ffi::{CStr, c_int, c_char},
-	ptr::null_mut,
-	fs::read
 };
+use tokio::{net::TcpListener, sync::oneshot};
 use ygopru::{
 	ygopro::{
-		Configuration,
-		managers::{ deck_manager::{self, DeckManager}, config_manager::{self, ConfigManager}, data_manager::{self, DataManager} },
-		single_duel::SingleDuelHost,
-		plugin::replay::{NAME, Configuration as PluginConfiguration}
+		DuelHost,
+		cli::{build_duel_host, start_local_server_with_listener},
+		managers::{
+			config_manager::{self, ConfigManager},
+			data_manager::{self, DataManager},
+			deck_manager::{self, DeckManager},
+		},
 	},
 	ygopro_core_wrapper::{
+		get_log_message, random::SEED_COUNT, set_card_reader, set_message_handler,
 		set_script_reader,
-        set_card_reader,
-        set_message_handler,
-		get_log_message,
-		DuelSeed,
-		random::SEED_COUNT
 	},
 	ygopro_data::{
-		data::{ReplayMode, Card, CoreCard},
-		message::{HostInfo, ctos},
-		constants::{MasterRule, Mode, Rule, Category, OT, Type, Race, Attribute, Linkmarkers}
+		constants::{Attribute, Category, Linkmarkers, MasterRule, Mode, OT, Race, Rule, Type},
+		data::{Card, CoreCard, ReplayMode},
+		message::HostInfo,
 	},
-	ygopro_handler::RoomProvider
 };
 
 static SERVER_CONTROL: OnceLock<Mutex<Option<ServerControl>>> = OnceLock::new();
-static SERVER_SEEDS: OnceLock<Mutex<Vec<[u32; SEED_COUNT]>>> = OnceLock::new();
 static SCRIPT_BUFFER: Mutex<[u8; 0x100000]> = Mutex::new([0u8; 0x100000]);
 
 struct ServerControl {
@@ -50,7 +39,7 @@ struct ServerControl {
 	server_thread: JoinHandle<()>,
 }
 
-pub fn start_server (
+pub fn start_server(
 	lflist: u32,
 	rule: u8,
 	mode: u8,
@@ -61,9 +50,9 @@ pub fn start_server (
 	start_lp: u32,
 	start_hand: u8,
 	draw_count: u8,
-	time_limit: u16
+	time_limit: u16,
 ) -> Result<u16, Error> {
-	let seeds: Vec<[u32; 8]>= Vec::new();
+	let seeds: Vec<[u32; SEED_COUNT]> = Vec::new();
 	let replay_mode: ReplayMode = ReplayMode::from_bits_retain(replay_mode);
 	let duel_rule: MasterRule = if duel_rule {
 		MasterRule::MasterRuleNew
@@ -75,48 +64,62 @@ pub fn start_server (
 	} else {
 		Mode::try_from(mode).unwrap_or(Mode::Single)
 	};
-    let host_info: HostInfo = HostInfo {
-        lflist: lflist,
-        rule: Rule::try_from(rule).unwrap_or(Rule::All),
-        duel_rule,
-        no_check_deck,
-        no_shuffle_deck,
-        start_lp,
-        start_hand,
-        draw_count,
-        time_limit,
-        mode
-    };
+	let host_info: HostInfo = HostInfo {
+		lflist: lflist,
+		rule: Rule::try_from(rule).unwrap_or(Rule::All),
+		duel_rule,
+		no_check_deck,
+		no_shuffle_deck,
+		start_lp,
+		start_hand,
+		draw_count,
+		time_limit,
+		mode,
+	};
 
-	let server_control_lock = SERVER_CONTROL.get_or_init(|| Mutex::new(None));
-	let mut server_control = server_control_lock.lock();
-	if let Some(i) = server_control.take() {
+	let server_control_lock: &Mutex<Option<ServerControl>> =
+		SERVER_CONTROL.get_or_init(|| Mutex::new(None));
+	let mut server_control: MutexGuard<'_, Option<ServerControl>> = server_control_lock.lock();
+	let old_server_control: Option<ServerControl> = server_control.take();
+	if let Some(i) = old_server_control {
+		let i: ServerControl = i;
 		i.shutdown_sender.send(()).ok();
 		i.server_thread.join().ok();
 	}
 
-	*SERVER_SEEDS.get_or_init(|| Mutex::new(Vec::new())).lock() = seeds.clone();
-
-	let (shutdown_sender, shutdown_receiver) = oneshot::channel();
-	let (start_result_sender, start_result_receiver) = std::sync::mpsc::channel();
-	let server_thread = std::thread::spawn(move || {
-		let runtime = match tokio::runtime::Builder::new_multi_thread()
+	let (shutdown_sender, shutdown_receiver): (oneshot::Sender<()>, oneshot::Receiver<()>) =
+		oneshot::channel();
+	let (start_result_sender, start_result_receiver): (
+		std::sync::mpsc::Sender<Result<u16, i32>>,
+		std::sync::mpsc::Receiver<Result<u16, i32>>,
+	) = std::sync::mpsc::channel();
+	let server_thread: JoinHandle<()> = std::thread::spawn(move || {
+		let runtime: tokio::runtime::Runtime = match tokio::runtime::Builder::new_multi_thread()
 			.enable_all()
 			.build()
 		{
 			Ok(runtime) => runtime,
 			Err(_) => {
-				start_result_sender.send(Err(- 1)).ok();
+				start_result_sender.send(Err(-1)).ok();
 				return;
 			}
 		};
 
 		runtime.block_on(async move {
-			if let Err(_) = init().await {
-				start_result_sender.send(Err(- 1)).ok();
+			let init_result: Result<(), Error> = init().await;
+			if let Err(_) = init_result {
+				start_result_sender.send(Err(-1)).ok();
 				return;
 			}
-			let _ = run_tcp_server(replay_mode, host_info, shutdown_receiver, start_result_sender).await;
+			let run_result: Result<(), Error> = run_tcp_server(
+				replay_mode,
+				host_info,
+				seeds,
+				shutdown_receiver,
+				start_result_sender,
+			)
+			.await;
+			let _: Result<(), Error> = run_result;
 		});
 		runtime.shutdown_timeout(Duration::from_secs(2));
 	});
@@ -125,117 +128,80 @@ pub fn start_server (
 		shutdown_sender,
 		server_thread,
 	});
-	start_result_receiver.recv()?
-		.map_err(|_|anyhow!("ygoserver runtime error"))
+	start_result_receiver
+		.recv()?
+		.map_err(|_| anyhow!("ygoserver runtime error"))
 }
 
-pub fn stop_server () {
-	let server_control = {
-		let server_control_lock = SERVER_CONTROL.get_or_init(|| Mutex::new(None));
+pub fn stop_server() {
+	let server_control: Option<ServerControl> = {
+		let server_control_lock: &Mutex<Option<ServerControl>> =
+			SERVER_CONTROL.get_or_init(|| Mutex::new(None));
 		server_control_lock.lock().take()
 	};
 
 	if let Some(server_control) = server_control {
+		let server_control: ServerControl = server_control;
 		server_control.shutdown_sender.send(()).ok();
 		server_control.server_thread.join().ok();
 	}
 }
 
-async fn run_tcp_server (
+async fn run_tcp_server(
 	replay_mode: ReplayMode,
 	host_info: HostInfo,
-	mut shutdown_receiver: oneshot::Receiver<()>,
-	start_result_sender: std::sync::mpsc::Sender<Result<u16, i32>>
+	seeds: Vec<[u32; SEED_COUNT]>,
+	shutdown_receiver: oneshot::Receiver<()>,
+	start_result_sender: std::sync::mpsc::Sender<Result<u16, i32>>,
 ) -> Result<(), Error> {
 	let listener: TcpListener = TcpListener::bind("0.0.0.0:0").await?;
 	let port: u16 = listener.local_addr()?.port();
 
 	start_result_sender.send(Ok(port)).ok();
 
-    let mut configuration: Configuration = Configuration::default();
-	configuration.seed_generator = Some(Box::new(seed_generator));
-    configuration.enable_plugin_with_configuration(NAME, PluginConfiguration { mode: replay_mode });
-	let (mut duel, duel_handle) = SingleDuelHost::new(host_info, configuration);
-	let mut client_tasks = Vec::new();
-
-	loop {
-		tokio::select! {
-			_ = &mut shutdown_receiver => {
-				break;
-			}
-			accepted = listener.accept() => {
-				let (stream, _address) = accepted?;
-				let (reader, writer) = stream.into_split();
-				let framed_read = LengthDelimitedCodec::builder()
-					.length_field_type::<u16>()
-					.little_endian()
-					.new_read(reader);
-				let mut framed_write = LengthDelimitedCodec::builder()
-					.length_field_type::<u16>()
-					.little_endian()
-					.new_write(writer);
-
-				let client_to_server_stream = framed_read.filter_map(|result| match result {
-					Ok(frame) => {
-						let mut cursor = Cursor::new(&frame);
-						ctos::Message::read_le(&mut cursor).ok()
-					}
-					Err(_) => None,
-				});
-
-				let mut server_to_client_stream = duel.add(client_to_server_stream);
-
-				client_tasks.retain(|task: &tokio::task::JoinHandle<()>| !task.is_finished());
-				let client_task = tokio::spawn(async move {
-					while let Some(message) = server_to_client_stream.next().await {
-						framed_write.send(message.data).await.ok();
-					}
-				});
-				client_tasks.push(client_task);
-			}
+	let duel: DuelHost = build_duel_host(host_info, replay_mode, seeds);
+	tokio::select! {
+		_ = shutdown_receiver => {
+		}
+		_ = start_local_server_with_listener(listener, duel) => {
 		}
 	}
 
-	duel_handle.abort();
-	duel_handle.await.ok();
-	for client_task in client_tasks {
-		client_task.abort();
-		client_task.await.ok();
-	}
 	Ok(())
 }
 
-async fn init () -> Result<(), Error> {
-	let mut hash_map: HashMap<u32, Card> = HashMap::new();
-	let cards = ygopro3_game::get::cards().await?;
+async fn init() -> Result<(), Error> {
+	let mut data_manager: DataManager = DataManager::new();
+	let cards: Vec<ygopro3_card::Card> = ygopro3_game::get::cards().await?;
 	for i in cards {
-		hash_map.insert(i.code, Card {
-			card: CoreCard {
-				code: i.code,
-				alias: i.alias,
-				setcode: i.setcode,
-				card_type: Type::from_bits_retain(i.card_type),
-				level: i.level,
-				attribute: Attribute::from_bits_retain(i.attribute),
-				race: Race::from_bits_retain(i.race),
-				attack: i.attack,
-				defense: i.defense,
-				left_scale: i.lscale,
-				right_scale: i.rscale,
-				link_marker: Linkmarkers::from_bits_retain(i.link_marker),
-				rule_code: 0
+		let i: ygopro3_card::Card = i;
+		data_manager.cards.insert(
+			i.code,
+			Card {
+				card: CoreCard {
+					code: i.code,
+					alias: i.alias,
+					setcode: i.setcode,
+					card_type: Type::from_bits_retain(i.card_type),
+					level: i.level,
+					attribute: Attribute::from_bits_retain(i.attribute),
+					race: Race::from_bits_retain(i.race),
+					attack: i.attack,
+					defense: i.defense,
+					left_scale: i.lscale,
+					right_scale: i.rscale,
+					link_marker: Linkmarkers::from_bits_retain(i.link_marker),
+					rule_code: 0,
+				},
+				ot: OT::from_bits_retain(i.ot),
+				category: Category::from_bits_retain(i.category),
+				name: i.name,
+				text: i.desc,
+				desc: i.hint,
 			},
-			ot: OT::from_bits_retain(i.ot),
-			category: Category::from_bits_retain(i.category),
-			name: i.name,
-			text: i.desc,
-			desc: i.hint
-		});
+		);
 	}
-	let data_manager: DataManager = DataManager {
-		cards: hash_map,
-		extra_setcode: HashMap::new()
-	};
+	data_manager.finalize_db();
 	let deck_manager: DeckManager = DeckManager::new();
 	let config_manager: ConfigManager = ConfigManager::new();
 	config_manager::set_global(config_manager);
@@ -249,8 +215,8 @@ async fn init () -> Result<(), Error> {
 	Ok(())
 }
 
-extern "C" fn script_reader (script_path: *const c_char, slen: *mut c_int) -> *mut u8 {
-	fn read_file (file_path: &str, buffer: &mut [u8]) -> Result<usize, Error> {
+extern "C" fn script_reader(script_path: *const c_char, slen: *mut c_int) -> *mut u8 {
+	fn read_file(file_path: &str, buffer: &mut [u8]) -> Result<usize, Error> {
 		let data: Vec<u8> = read(file_path)?;
 		let len: usize = data.len();
 		if len >= buffer.len() {
@@ -263,8 +229,8 @@ extern "C" fn script_reader (script_path: *const c_char, slen: *mut c_int) -> *m
 	if script_path.is_null() || slen.is_null() {
 		return null_mut();
 	}
-	let path = unsafe { CStr::from_ptr(script_path).to_string_lossy() };
-	let mut buffer = SCRIPT_BUFFER.lock();
+	let path: Cow<'_, str> = unsafe { CStr::from_ptr(script_path).to_string_lossy() };
+	let mut buffer: MutexGuard<'_, [u8; 0x100000]> = SCRIPT_BUFFER.lock();
 
 	if path.starts_with("./script") {
 		(move || -> Result<*mut u8, Error> {
@@ -275,33 +241,35 @@ extern "C" fn script_reader (script_path: *const c_char, slen: *mut c_int) -> *m
 				Err(anyhow!("too long memory"))
 			} else {
 				buffer[..len].copy_from_slice(&data);
-				unsafe { *slen = len as c_int; }
+				unsafe {
+					*slen = len as c_int;
+				}
 				Ok(buffer.as_mut_ptr())
 			}
-		})().unwrap_or(null_mut())
+		})()
+		.unwrap_or(null_mut())
 	} else {
 		(move || -> Result<*mut u8, Error> {
 			let len: usize = read_file(path.as_ref(), &mut *buffer)?;
-			unsafe { *slen = len as c_int; }
+			unsafe {
+				*slen = len as c_int;
+			}
 			Ok(buffer.as_mut_ptr())
-		})().unwrap_or(null_mut())
+		})()
+		.unwrap_or(null_mut())
 	}
 }
 
-fn seed_generator (duel_count: u8) -> DuelSeed {
-	let seed = SERVER_SEEDS
-		.get()
-		.and_then(|seeds| seeds.lock().get(duel_count as usize).copied());
-	match seed {
-		Some(seed) => DuelSeed::Complicated(seed),
-		None => DuelSeed::None,
+extern "C" fn core_message_handler(pduel: isize, message_type: u32) -> u32 {
+	let mut buffer: [u8; 1024] = [0u8; 1024];
+	unsafe {
+		get_log_message(pduel, buffer.as_mut_ptr());
 	}
-}
-
-extern "C" fn core_message_handler (pduel: isize, message_type: u32) -> u32 {
-	let mut buffer = [0u8; 1024];
-	unsafe { get_log_message(pduel, buffer.as_mut_ptr()); }
-	let c_message = unsafe { CStr::from_ptr(buffer.as_ptr() as *const c_char) };
-	println!("core message[{}]: {}", message_type, c_message.to_string_lossy());
+	let c_message: &CStr = unsafe { CStr::from_ptr(buffer.as_ptr() as *const c_char) };
+	println!(
+		"core message[{}]: {}",
+		message_type,
+		c_message.to_string_lossy()
+	);
 	0
 }
