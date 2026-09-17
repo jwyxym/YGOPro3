92ee9de2eab165a35d550752ec2dacda4448d5e6
jwyxym <jwyxym@126.com>
2026-09-17 12:55:01 +0800

    修改脚本读取

--- src-tauri/tauri-plugin-ygopro3/crates/ygopro3_scripts/src/lib.rs
+++ src-tauri/tauri-plugin-ygopro3/crates/ygopro3_scripts/src/lib.rs
@@ -1,17 +1,8 @@
-use ygopro3_fs::File;
+use std::collections::BTreeMap;
 
-use std::{collections::BTreeMap, path::Path};
-use serde::Serialize;
-use walkdir::WalkDir;
-
-#[derive(Serialize, Clone, Debug)]
-pub enum ScriptContent {
-	Path(String),
-	ZipFile(usize)
-}
-#[derive(Serialize, Clone, Debug)]
+#[derive(Clone, Debug)]
 pub struct Script {
-	content: BTreeMap<String, ScriptContent>,
+	content: BTreeMap<String, usize>,
 }
 impl Script {
 	pub fn new () -> Self {
@@ -20,33 +11,15 @@ impl Script {
 		}
 	}
 
-	pub fn insert (&mut self, key: String, value: ScriptContent) -> () {
+	pub fn insert (&mut self, key: String, value: usize) -> () {
 		self.content.insert(key, value);
 	}
 
-	pub fn get (&self, key: &str) -> Option<&ScriptContent> {
+	pub fn get (&self, key: &str) -> Option<&usize> {
 		self.content.get(key)
 	}
 
-	pub fn read_dir<P: AsRef<Path>> (mut self, path: P) -> Self {
-		WalkDir::new(path)
-			.max_depth(1)
-			.into_iter()
-			.filter_map(|i| {
-				if let Ok(i) = i {
-					let file: File = File::new(i.path())?;
-					if file.ext() == "lua" {
-						return Some((String::from(file.name()), String::from(file.path())));
-					}
-				}
-				None
-			})
-			.for_each(|i| {
-				self.content.insert(i.0, ScriptContent::Path(i.1));
-			});
-		self
-	}
-	pub fn to_array (&self) -> Vec<(String, ScriptContent)> {
+	pub fn to_array (&self) -> Vec<(String, usize)> {
 		self.content.clone().into_iter().collect()
 	}
 }
